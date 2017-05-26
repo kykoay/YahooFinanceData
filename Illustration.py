7f4030b55609704619d164161e236660c20e552a
kykoay <kykoay1011@gmail.com>
2017-05-25 22:11:50 -0400

    Module to access Yahoo Finance Data
    On May 16th changes on Yahoo Finance impacted popular modules like
    pandas's data reader. Based on the pointers of Jev Kuznetsov I
    implemented his solution on this module to retrieve Yahoo Finance data.
    May this help you in mitigating the impact from May 16th

---
+++ Illustration.py
@@ -0,0 +1,12 @@
+#############Illustration of how to use this module
+from YFData import *
+
+
+check = YahooPrice('2017-05-01','2017-05-20',"XCV.TO")
+
+print check.ticker
+print check.data
+print check.df.tail(n=5) #Note if you remove .tail(n=5) you get all the data in a dataframe format for the range of date you specified
+
+CADUSD = YahooFx('2017-05-23',"CAD=X") 
+print CADUSD.rawprice
---
+++ YFData.py
@@ -0,0 +1,41 @@
+import urllib
+from datetime import datetime, timedelta
+import requests
+import datetime as dt
+import string
+import time
+import numpy as np
+import pandas as pd
+import re
+import json
+import io
+
+######################################
+### Reference: 
+### https://github.com/sjev/trading-with-python/blob/master/scratch/get_yahoo_data.ipynb
+### Jev Kuznetsov described what happened and provided an excellent tutorial on how to retrieve the data.
+### Foreign currency prices are accessed differently as historical data is not provided. 
+#####################################
+
+class YahooPrice(object):
+	def __init__(self,datefrom,dateto,ticker):
+		self.datefrom = np.datetime64(datefrom)  #Date from must be in tuple format. Eg 2017 May 21st is 
+		self.dateto = np.datetime64(dateto)
+		self.ticker = ticker
+		self.datefromsec = int((self.datefrom - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's') )
+		self.datetosec = int((self.dateto - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')  )
+		self.url = requests.get('https://finance.yahoo.com/quote/'+ self.ticker +'/history')
+		self.crumb = self.url.text.split('"CrumbStore"')[1].split('"crumb":')[1].split(',')[0].replace('}','').replace('"','')
+		self.cookie = self.url.cookies['B']
+		self.data = requests.get('https://query1.finance.yahoo.com/v7/finance/download/' + self.ticker +'?period1={0}&period2={1}&interval=1d&events=history&crumb={2}'.format(self.datefromsec,self.datetosec,self.crumb),cookies={'B':self.cookie})
+		self.buf = io.StringIO(self.data.text)
+		self.df = pd.read_csv(self.buf,index_col = 0 )
+
+class YahooFx(object):
+	def __init__ (self,fxticker):
+		self.fxticker = fxticker
+		self.url = urllib.urlopen('https://ca.finance.yahoo.com/quote/{0}/?p={1}'.format(self.fxticker,self.fxticker)).read()
+		self.rawprice = self.url.split('"bid":{"raw":')[1].split(',"fmt"')[0]
+		###Note there are other data points of interest, you can choose to parse them out from the returned html that has json in it.Eg Previous Close price etc
+
+
